895fb067a147142cfdc257533db51c00c09f9f8d
Mayukha Tellabati <mayukha1tellabati@gmail.com>
2025-02-22 20:17:06 +0530

    third commit

--- src/App.js
+++ src/App.js
@@ -6,37 +6,37 @@ import { useEffect, useState } from "react";
 export default function App() {
   
 
-  const [data, setData] = useState([]);
+  const [employeeData, setEmployeeData] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
   //const [totalPages, setTotalPages] = useState(10);
-  const [itemsPerPage, setItemPerPage] = useState(10);
-  const fetchData = async () => {
+  const [itemsPerPage, setItemsPerPage] = useState(10);
+  const fetchEmployeeData = async () => {
     try {
       const response = await fetch("https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json");
       const data = await response.json();
       // console.log(data);
-      setData(data);
+      setEmployeeData(data);
       //setTotalPages(Math.ceil(data.length / 10)); // to divide no. of pages
     } catch (error) {
-      console.log("Failed fetchind data:", error);
+      console.log("Failed to fetch data:", error);
       alert("failed to fetch data");
     }
   };
-  useEffect(() => fetchData(), []);
+  useEffect(() => fetchEmployeeData(), []);
 
   const handlePrevious = () => {
     if (currentPage > 1) setCurrentPage((prev) => prev - 1);
   };
 
   const handleNext = () => {
-    if (currentPage < Math.ceil(data.length/itemsPerPage)) setCurrentPage((prev) => prev + 1);
+    if (currentPage < Math.ceil(employeeData.length/itemsPerPage)) setCurrentPage((prev) => prev + 1);
   };
 
   const getPaginatedData = () => {
     //to divide data in each page
     let endIndex = currentPage * itemsPerPage;
     let startIndex = endIndex-itemsPerPage;
-    return data.slice(startIndex, endIndex);
+    return employeeData.slice(startIndex, endIndex);
   };
 
   return (
@@ -65,7 +65,7 @@ export default function App() {
           Previous
         </button>
         <span>{currentPage}</span>
-        <button disabled={currentPage >= Math.ceil(data.length.itemsPerPage)} onClick={handleNext}>
+        <button disabled={currentPage >= Math.ceil(employeeData.length.itemsPerPage)} onClick={handleNext}>
           Next
         </button>
       </div>
